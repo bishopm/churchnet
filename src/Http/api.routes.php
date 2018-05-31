855d7c270df7f19e21fdd524f9d30040f2143579
Michael Bishop <michael@goodworx.co.za>
2018-05-31 13:08:12 +0200

    Allow society show

--- src/Http/api.routes.php
+++ src/Http/api.routes.php
@@ -15,6 +15,7 @@ Route::middleware(['handlecors','bindings'])->group(function () {
 
     // Societies
     Route::get('/api/methodist/circuits/{circuit}/societies', ['uses'=>'Bishopm\Churchnet\Http\Controllers\Api\SocietiesController@index','as'=>'api.societies.index']);
+    Route::get('api/methodist/circuits/{circuit}/societies/{society}', ['uses'=>'Bishopm\Churchnet\Http\Controllers\Api\SocietiesController@show','as'=>'api.societies.show']);
     Route::group(['middleware' => ['jwt.auth','handlecors']], function () {
         Route::get('api/methodist/check', ['uses'=>'Bishopm\Churchnet\Http\Controllers\Auth\ApiAuthController@check','as'=>'api.check']);
 
@@ -55,7 +56,6 @@ Route::middleware(['handlecors','bindings'])->group(function () {
 
         // Societies
         Route::get('api/methodist/circuits/{circuit}/societies/create', ['uses'=>'Bishopm\Churchnet\Http\Controllers\Api\SocietiesController@create','as'=>'api.societies.create']);
-        Route::get('api/methodist/circuits/{circuit}/societies/{society}', ['uses'=>'Bishopm\Churchnet\Http\Controllers\Api\SocietiesController@show','as'=>'api.societies.show']);
         Route::post('api/methodist/circuits/{circuit}/societies', ['uses'=>'Bishopm\Churchnet\Http\Controllers\Api\SocietiesController@store','as'=>'api.societies.store']);
         Route::get('api/methodist/circuits/{circuit}/societies/{society}/edit', ['uses'=>'Bishopm\Churchnet\Http\Controllers\Api\SocietiesController@edit','as'=>'api.societies.edit']);
         Route::put('api/methodist/circuits/{circuit}/societies/{society}', ['uses'=>'Bishopm\Churchnet\Http\Controllers\Api\SocietiesController@update','as'=>'api.societies.update']);
