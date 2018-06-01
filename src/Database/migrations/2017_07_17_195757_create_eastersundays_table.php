03a8b5df4875af74c42a5e4942886d735653fa7b
Michael Bishop <michael@goodworx.co.za>
2018-06-01 12:57:17 +0200

    Lectionary tweaks

--- src/Database/migrations/2017_07_17_195757_create_eastersundays_table.php
+++ src/Database/migrations/2017_07_17_195757_create_eastersundays_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateReadingsTable extends Migration
+class CreateEastersundaysTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,13 +12,9 @@ class CreateReadingsTable extends Migration
      */
     public function up()
     {
-        Schema::create('readings', function (Blueprint $table) {
+        Schema::create('eastersundays', function (Blueprint $table) {
             $table->increments('id');
-            $table->date('ldate');
-            $table->string('description');
-            $table->string('a');
-            $table->string('b');
-            $table->string('c');
+            $table->date('eastersunday');
         });
     }
 
@@ -29,6 +25,6 @@ class CreateReadingsTable extends Migration
      */
     public function down()
     {
-        Schema::drop('readings');
+        Schema::drop('eastersundays');
     }
 }
--- src/Http/Controllers/Api/LectionaryController.php
+++ src/Http/Controllers/Api/LectionaryController.php
@@ -15,9 +15,13 @@ class LectionaryController extends Controller
         $this->reading = $reading;
     }
 
-    public function sunday()
+    public function sunday($date="")
     {
-        $this->sunday = strtotime(date('Y-m-d', strtotime('sunday')));
+        if ($date=="") {
+            $this->sunday = strtotime(date('Y-m-d', strtotime('sunday')));
+        } else {
+            $this->sunday = strtotime($date);
+        }
         $this->lectionaryYear();
         $this->setUpArray();
         $this->buildYear();
@@ -47,6 +51,11 @@ class LectionaryController extends Controller
         return strtotime('last Thursday of November ' . $yr . ' + 3 days');
     }
 
+    private function epiphany($yr)
+    {
+        return strtotime('first Sunday of January ' . $yr);
+    }
+
     private function setUpArray()
     {
         $this->data[0]['date']=date("Y-m-d", $this->advent);
@@ -54,6 +63,9 @@ class LectionaryController extends Controller
         while (strtotime($this->data[$i-1]['date']) < $this->nextAdvent-604800) {
             $this->data[$i]['date']=date("Y-m-d", (604800+strtotime($this->data[$i-1]['date'])));
             $i++;
+        }
+        foreach ($this->data as $key=>$dat) {
+            $this->weeks[$dat['date']]=$key;
         }
     }
 
@@ -65,9 +77,40 @@ class LectionaryController extends Controller
         $this->data[2]['lection']=$this->reading->locate($this->lyear, 'Advent 3');
         $this->data[3]['lection']=$this->reading->locate($this->lyear, 'Advent 4');
 
+        $this->data[4]['lection']=$this->reading->locate($this->lyear, 'First Sunday after Christmas');
+        $this->data[5]['lection']=$this->reading->locate($this->lyear, 'Epiphany');
+        if (substr(date('Y-m-d', $this->epiphany($this->yr)), 9, 1)<>'7') {
+            $this->data[6]['lection']=$this->reading->locate($this->lyear, 'First Sunday after the Epiphany (Ordinary 1)');
+            $ndx=7;
+        } else {
+            $ndx=6;
+        }
+        $this->data[$ndx]['lection']=$this->reading->locate($this->lyear, 'Second Sunday after the Epiphany (Ordinary 2)');
+
         // Lent
-        $x = array_search('2018-04-01', $this->data);
-        dd($x);
-        dd($this->data);
+        $eastersunday = DB::table('eastersundays')->whereRaw('SUBSTRING(eastersunday, 1,  4) = '.$this->yr)->first()->eastersunday;
+        $this->data[$this->weeks[$eastersunday]]['lection']=$this->reading->locate($this->lyear, 'Easter Sunday');
+        $this->data[-1+$this->weeks[$eastersunday]]['lection']=$this->reading->locate($this->lyear, 'Fifth Sunday in Lent');
+        $this->data[-2+$this->weeks[$eastersunday]]['lection']=$this->reading->locate($this->lyear, 'Fourth Sunday in Lent');
+        $this->data[-3+$this->weeks[$eastersunday]]['lection']=$this->reading->locate($this->lyear, 'Third Sunday in Lent');
+        $this->data[-4+$this->weeks[$eastersunday]]['lection']=$this->reading->locate($this->lyear, 'Second Sunday in Lent');
+        $this->data[-5+$this->weeks[$eastersunday]]['lection']=$this->reading->locate($this->lyear, 'First Sunday in Lent');
+        $this->testData();
+    }
+
+    private function testData()
+    {
+        $fin ="<h1>" . $this->yr . "</h1><table>";
+        foreach ($this->data as $key=>$dat) {
+            $fin.= "<tr>";
+            $fin.= "<td>" . $key . "</td>";
+            $fin.= "<td>" . $dat['date'] . "</td>";
+            if (isset($dat['lection'])) {
+                $fin.= "<td>" . $dat['lection']['description'] . "</td>";
+            }
+            $fin.= "</tr>";
+        }
+        $fin.= "</table>";
+        echo $fin;
     }
 }
