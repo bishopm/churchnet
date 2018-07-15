d1c63b810d6292acf823d575018fdf6698a83df1
Michael Bishop <michael@goodworx.co.za>
2018-07-15 21:45:11 +0200

    Backend theme

--- src/Resources/views/auth/login.blade.php
+++ src/Resources/views/auth/login.blade.php
@@ -7,12 +7,41 @@
             <div class="card">
                 <div class="card-body text-center">
                     <h3>Login to ChurchNet</h3>
+                    <p>Log in using any one of the three options below</p>
                     <div class="row">
                         <div class="col-md-12">
-                            <p class="text-center">using Facebook or Google:<br><br>
-                            <a href="{{ url('redirect/facebook') }}" style="color:#3b5998;"><i class="fab fa-facebook fa-4x"></i></a>&nbsp;
-                            <a href="{{ url('redirect/google') }}" style="color:#db3236;"><i class="fab fa-google-plus-square fa-4x"></i></a></p>
-                            <p class="text-center"><small>We don't use your social media details at all - just the authentication mechanism.<br>And in the process, save you remembering another username and password :)</small></p>
+                        <ul class="nav nav-pills nav-fill">
+                            <li class="nav-item">
+                                <a class="nav-link active" data-toggle="tab" href="#social">Social Login</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" data-toggle="tab" href="#username">Username</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" data-toggle="tab" href="#phone">Cellphone</a>
+                            </li>
+                        </ul>
+                        <div class="tab-content" id="myTabContent">
+                            <div class="tab-pane fade show active" id="social" role="tabpanel" aria-labelledby="home-tab">
+                                <a href="{{ url('redirect/facebook') }}" style="color:#3b5998;"><i class="fab fa-facebook fa-4x"></i></a>&nbsp;
+                                <a href="{{ url('redirect/google') }}" style="color:#db3236;"><i class="fab fa-google-plus-square fa-4x"></i></a></p>
+                            </div>
+                            <div class="tab-pane fade" id="username" role="tabpanel" aria-labelledby="profile-tab">
+                                <div class="mt-3 form-group">
+                                    <input class="form-control" placeholder="Username" name="name">
+                                </div>
+                                <div class="form-group">
+                                    <input class="form-control" placeholder="Password" name="password" type="password">
+                                </div>
+                            </div>
+                            <div class="tab-pane fade" id="phone" role="tabpanel" aria-labelledby="contact-tab">
+                                <div class="mt-3 form-group">
+                                    <input class="form-control" placeholder="Cellphone" name="phone">
+                                </div>
+                                <div class="form-group">
+                                    <input class="form-control" placeholder="Password" name="password" type="password">
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
