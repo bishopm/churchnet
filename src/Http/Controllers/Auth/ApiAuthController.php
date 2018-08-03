37d99ad13a61402d672585ffbb0b7c30ec84c28d
Michael Bishop <michael@goodworx.co.za>
2018-08-03 14:17:33 +0200

    API auth

--- src/Http/Controllers/Auth/ApiAuthController.php
+++ src/Http/Controllers/Auth/ApiAuthController.php
@@ -37,23 +37,14 @@ class ApiAuthController extends Controller
 
     public function journeylogin(Request $request)
     {
-        // grab credentials from the request
-        $credentials = $request->only('name');
-        $user=User::where('name', $request->input('phone'))->first();
-        //$fullname=$user->individual->firstname . " " . $user->individual->surname;
-        //$indiv_id=$user->individual_id;
-        //Log::info('API login attempt: ' . json_encode($credentials));
+        $user=User::where('phone', $request->name)->first();
         try {
-            // attempt to verify the credentials and create a token for the user
             if (!$token=JWTAuth::fromUser($user)) {
                 return response()->json(['error' => 'invalid_credentials'], 401);
             }
         } catch (JWTException $e) {
-            // something went wrong whilst attempting to encode the token
             return response()->json(['error' => 'could_not_create_token'], 500);
         }
-
-        // all good so return the token
         return response()->json(compact('token'));
     }
 
