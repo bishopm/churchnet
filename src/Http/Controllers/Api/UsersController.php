506c748c22b05e495b3bbebcf48f5388201bb412
Michael Bishop <michael@goodworx.co.za>
2018-08-23 22:06:38 +0200

    API

--- src/Http/Controllers/Api/UsersController.php
+++ src/Http/Controllers/Api/UsersController.php
@@ -11,7 +11,20 @@ class UsersController extends Controller
 {
     public function userdetails($id)
     {
-        return User::with('districts', 'circuits', 'societies')->where('id', $id)->first();
+        $data = User::with('districts', 'circuits', 'societies')->where('id', $id)->first();
+        $user = array();
+        foreach ($data->circuits as $circuit) {
+            $user['circuits'][$circuit->pivot->permission][]=$circuit->id;
+        }
+        foreach ($data->societies as $society) {
+            $user['societies'][$society->pivot->permission][]=$society->id;
+        }
+        foreach ($data->districts as $district) {
+            $user['districts'][$district->pivot->permission][]=$district->id;
+        }
+        $user['id'] = $data->id;
+        $user['name'] = $data->name;
+        return $user;
     }
 
     public function index()
