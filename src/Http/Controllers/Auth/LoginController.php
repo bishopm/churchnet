cfe53439f323d8a9db57a21e6b419aa0d059ad2b
Michael Bishop <admin@church.net.za>
2020-06-21 15:29:14 +0200

    Update composer

--- src/Http/Controllers/Auth/LoginController.php
+++ src/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,6 @@ namespace Bishopm\Churchnet\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
